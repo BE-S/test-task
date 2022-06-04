1c9bb47480156c5f347cfd53d1df939b531242a7
Danil <danil.dogi000.dogi000@gmail.com>
2022-06-04 23:48:12 +0700

    Refactoring code

--- database.php
+++ database.php
@@ -2,44 +2,52 @@
 
 if ($_POST["sql_code"] != NULL)
 {
-    read($_POST["sql_code"]);
+    $conn = new PDO("mysql:host=localhost:3307; dbname=workers", "root", "root");
+    $req = $conn->prepare($_POST["sql_code"]);
+    $req->execute();
+    $result = $req->fetchAll(PDO::FETCH_ASSOC);
+    
+    $DataTable = new DataTable($result, $req);
+    $DataTable->GetTable();
 }
 
-function read($sql) 
+Class DataTable
 {
-    $table_array = array();
-    $db_host = "localhost:3307";
-    $db_login = "root";
-    $db_password = "root";
-    $db_name = "workers";
+    private $result;
+    private $req;
+    private $table_array = array();
 
-    try {
-        $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_login, $db_password);
-        $req = $conn->prepare($sql);
-        $req->execute();
-        $result = $req->fetchAll(PDO::FETCH_ASSOC);
-
-        if(!empty($result))
-        {
-            array_push($table_array, $req->columnCount());
-            array_push($table_array, $req->rowCount());
-
-            foreach($result as $row_count => $row)
-            {
-                if ($row_count <= 0) {
-                    foreach($row as $count => $row_value) 
-                        array_push($table_array, $count);
-                }
-
-                foreach($row as $row_value) 
-                    array_push($table_array, $row_value);
-            }
-            echo json_encode($table_array);
-        } else {
-            array_push($table_array, 1, 1, "В данные таблице временно отсутствуют");
-            echo json_encode($table_array);
-        }
-    } catch(PDOException $e) {
-        echo $e;
+    function __construct($result, $req) 
+    {
+        $this->result = $result;
+        $this->req = $req;
     }
-};
+
+    public function GetTable() 
+    {
+        try {
+            if(!empty($this->result))
+            {
+                array_push($this->table_array, $this->req->columnCount());
+                array_push($this->table_array, $this->req->rowCount());
+    
+                foreach($this->result as $row_count => $row)
+                {
+                    if ($row_count <= 0) {
+                        foreach($row as $count => $row_value) 
+                            array_push($this->table_array, $count);
+                    }
+    
+                    foreach($row as $row_value) 
+                        array_push($this->table_array, $row_value);
+                }
+                echo json_encode($this->table_array);
+            } else {
+                array_push($this->table_array, 1, 1, "В данные таблице временно отсутствуют");
+                echo json_encode($this->table_array);
+            }
+        } catch(PDOException $e) {
+            echo $e;
+        }
+    }
+}
